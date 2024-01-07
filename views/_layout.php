0b5ac6519d37d90d52249cf37de35204e6055993
alifertah <alifertah42@gmail.com>
2024-01-07 20:22:41 +0100

    feat : table for categories

--- views/_layout.php
+++ views/_layout.php
@@ -35,12 +35,12 @@
             echo ("
             <div class='dropdown'>
             <button class='btn  dropdown-toggle' type='button' data-bs-toggle='dropdown' aria-expanded='false'>
-              Dropdown button
+              Action to categories
             </button>
             <ul class='dropdown-menu'>
-              <li><a class='dropdown-item' href='#'>Action</a></li>
-              <li><a class='dropdown-item' href='#'>Another action</a></li>
-              <li><a class='dropdown-item' href='#'>Something else here</a></li>
+              <li><a class='dropdown-item' href='#'>Create</a></li>
+              <li><a class='dropdown-item' href='#'>Edit</a></li>
+              <li><a class='dropdown-item' href='#'>Remove</a></li>
             </ul>
           </div>
             ");
---
+++ views/categories_view.php
@@ -0,0 +1,26 @@
+<div class="container">
+        <table class="table table-striped table-sm table-bordered">
+            <thead>
+            <tr class="text-center ">
+                <th class="text-center text-white font-weight-bold bg-dark">id</th>
+                <th class="text-center text-white font-weight-bold bg-dark">Fist name</th>
+                <th class="text-center text-white font-weight-bold bg-dark">Last Name</th>
+                <th class="text-center text-white font-weight-bold bg-dark">Email</th>
+                <th class="text-center text-white font-weight-bold bg-dark">Phone Number</th>
+                <th class="text-center text-white font-weight-bold bg-dark">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="text-center text-secondary">
+    <td>no3man</td>
+    <td>no3man</td>
+    <td>no3man</td>
+    <td>no3man</td>
+    <td>no3man</td>
+    <td>
+        <a href="#"><i class="fa-solid fa-trash"></i></a>
+        <a href="#"><i class="fa-solid fa-pen-to-square"></i></i></a>
+    </td>
+    </tr></tbody>
+    </table>
+</div>
