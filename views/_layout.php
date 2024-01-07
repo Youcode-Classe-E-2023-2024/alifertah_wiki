8ba73935390412e108d213fd39d01aa8a227d075
alifertah <alifertah42@gmail.com>
2024-01-07 16:19:30 +0100

    feat : init categories

--- views/_layout.php
+++ views/_layout.php
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">categories</a>
+          <a class="nav-link active" aria-current="page" href="categories">categories</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
