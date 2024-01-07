f94e0fb0844f8d3120e6bc92e187e3cad1ba5781
alifertah <alifertah42@gmail.com>
2024-01-07 17:12:46 +0100

    feat : add sweet alert cdn and redirect admin to his page

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -3,12 +3,16 @@ if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $stmt = $db->prepare("SELECT password FROM users WHERE email = ?");
+    $stmt = $db->prepare("SELECT * FROM users WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($password, $user['password'])) {
-        echo "Login successful!";
+        $_SESSION['user_id'] = $user['user_id'];
+        $_SESSION['user_type'] = $user['user_type'];
+        if($_SESSION['user_type'] == 'admin'){
+            header("Location: index.php?page=admin");
+        }
     } else {
         echo "Invalid credentials!";
     }
