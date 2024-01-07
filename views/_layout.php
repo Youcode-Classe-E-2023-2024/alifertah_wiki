3e35e2cd8928291b204509699a97a89be97457b5
alifertah <alifertah42@gmail.com>
2024-01-07 19:45:38 +0100

    feat : dropdown menu in categories page

--- views/_layout.php
+++ views/_layout.php
@@ -30,6 +30,22 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php?page=register">register</a>
         </li>
+        <?php 
+          if($_SESSION['user_type'] === 'admin' && $_GET['page'] === "categories"){
+            echo ("
+            <div class='dropdown'>
+            <button class='btn  dropdown-toggle' type='button' data-bs-toggle='dropdown' aria-expanded='false'>
+              Dropdown button
+            </button>
+            <ul class='dropdown-menu'>
+              <li><a class='dropdown-item' href='#'>Action</a></li>
+              <li><a class='dropdown-item' href='#'>Another action</a></li>
+              <li><a class='dropdown-item' href='#'>Something else here</a></li>
+            </ul>
+          </div>
+            ");
+          }
+        ?>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
